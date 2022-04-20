cabaa938736aa07934f589d0781a542a6cc5c74f
Henrique <pereira.henriquep@gmail.com>
2022-04-20 09:14:51 -0300

    ..

--- push.sh
+++ push.sh
@@ -1,17 +1,23 @@
 # Faz o git pull dos repositorios listados
 
 
-reps="latex_doutorado \
-	  ocean-wave \
-	  phd-wave-group \
-	  wave-climate-salinopolis \
-	  hpppereira.github.io \
-	  ladsin-br"
+reps="atmosmarine-bkp \
+      delfos-db \
+      ladsin-br \
+      omega-assurua \
+      omega-azcopy \
+      omega-db \
+      omega-ecmwf \
+      omega-mapi \
+      pnboia-db \
+      prevmet-db \
+      simcosta-db \
+      webdemo-db"
 
 for rep in $reps; do
-	echo $rep
-	cd "/home/hp/git/"$rep
-	git add .
-	git commit -m ".."
-	git push
+    echo $rep
+    cd "/home/hp/git/"$rep
+    git add .
+    git commit -m ".."
+    git push
 done
